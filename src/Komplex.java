fc88d447f10e0c9e4214b2366fca0f29e462e6fc
Mephit299 <94972436+Mephit299@users.noreply.github.com>
2023-03-14 15:11:46 +0100

    Gjor filen för programet

---
+++ src/Komplex.java
@@ -0,0 +1,16 @@
+public class Komplex {
+    public Komplex(double v, double v1) {
+
+
+    }
+
+    public Komplex add(Komplex b) {
+    }
+
+    public Komplex multiply(Komplex b) {
+    }
+
+    public double arg() {
+    }
+}
+
